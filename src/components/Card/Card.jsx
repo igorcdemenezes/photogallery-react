1f2ff14090dd9d6d91208e19230ec9b06a4887b9
igorcdemenezes <igorcdemenenezes@gmail.com>
2024-10-22 10:42:23 -0300

    Initial commit

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,9 @@
+import styles from "./Card.module.css";
+
+export default function Card() {
+  return (
+    <div className={styles.container}>
+      <p className={styles.p}>teste</p>
+    </div>
+  );
+}
