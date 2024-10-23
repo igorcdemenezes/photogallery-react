1cf3739e202879b3986fb2bc4405a53f3ab5c12c
igorcdemenezes <igorcdemenenezes@gmail.com>
2024-10-23 12:27:13 -0300

    feat: Header created Header created but unstyled

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 // import Card from "./components/Card/Card";
 import ProductList from "./controllers/ProductList";
+import Layout from "./view/Layout/Layout";
 
 function App() {
   return (
     <>
-      <ProductList />
+      <Layout />
     </>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <a>Logo</a>
+        <ul>
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Galeria</a>
+          </li>
+        </ul>
+        <input />
+      </nav>
+    </header>
+  );
+}
